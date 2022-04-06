7eeebec38bcd74ddad43a32f3285689857d27794
Mathews <mathewsrodrigues9@gmail.com>
2022-04-06 19:06:25 -0300

    finalizado o exercicoi 7

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicio2/exercicio7.js
@@ -0,0 +1,19 @@
+const nota = 160;
+
+if (nota >=90) {
+    console.log("Sua nota é A");
+} else if (nota >= 80) {
+    console.log("Sua nota é B");
+} else if (nota >= 70) {  
+    console.log("Sua nota é C");
+} else if (nota >= 60) {
+    console.log("Sua nota é D");
+} else if (nota >= 50) {
+    console.log("Sua nota é E");
+} else if (nota <50){
+    console.log("Sua nota é F");
+} else if (nota > 100 && nota < 0) {
+    console.log("Nota inválida");
+}
+
+
