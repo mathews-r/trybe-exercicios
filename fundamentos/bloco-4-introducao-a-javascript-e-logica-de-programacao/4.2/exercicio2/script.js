dcb0168b6c98761cc83cc4207246275d58dcee85
Mathews <mathewsrodrigues9@gmail.com>
2022-04-07 19:33:09 -0300

    ajuste nos exercicios

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/4.2/exercicio2/script.js
@@ -0,0 +1,109 @@
+// exercicio 1
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let numeros of numbers) {
+//     console.log(numeros);
+// }
+
+// exercicio 2
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let resultado = 0
+
+// for (let index = 0; index < numbers.length; index+=1) {
+//     resultado+= numbers[index];
+//     }  console.log(resultado);
+
+
+// exercicio 3
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let resultado = 0
+
+// for (let index = 0; index < numbers.length; index+=1) {
+//     resultado+= numbers[index];
+//     }
+//     console.log(resultado/numbers.length);
+
+// exercicio 4
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let resultado = 0
+
+// for (let index = 0; index < numbers.length; index+=1) {
+//     resultado+= numbers[index];
+//     }
+//     if(resultado/numbers.length > 20) {
+//         console.log("Maior que 20");
+//     } else {
+//     console.log("Menor que 20");
+//     }
+
+// exercicio 5
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let resultado = 0
+
+// for( let index = 0; index < numbers.length; index += 1){
+   
+//     if (numbers[index] > resultado) {
+//         resultado = numbers[index]; 
+        
+//     }
+// }  
+//    console.log(resultado);
+
+// exercicio 
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let pares = 0
+
+// for(let index = 0; index < numbers.length; index +=1){
+//     if (numbers[index] % 2 == 0){
+//         pares+=1 ;
+           
+//     }
+// }
+// console.log(pares);
+
+// exercicio 6
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let impares = 0
+
+// for(let index = 0; index < numbers.length; index +=1){
+//     if (numbers[index] % 2 > 0){
+//         impares+=1 ;
+           
+//     }
+// }
+// console.log(impares);
+
+// exercicio 7
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let resultado = Infinity;
+
+// for( let index = 0; index < numbers.length; index += 1){
+   
+//     if (numbers[index] < resultado) {
+//         resultado = numbers[index]; 
+        
+//     }
+// }  
+//    console.log(resultado);
+
+
+
+// exercicio 
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let impares = 0
+
+// for(let index = 0; index < numbers.length; index +=1){
+//     if (numbers[index] % 2 > 0){
+//         impares+=1 ;
+           
+//     }
+// }
+// console.log(impares);
+
+// exercicio 8
