3be102f35e2b1f8dfb423a281715a57a9a28e152
Mathews <mathewsrodrigues9@gmail.com>
2022-04-09 10:35:18 -0300

    resolucao de exercicios

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/4.2/exercicio1/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/4.2/exercicio1/script.js
@@ -20,8 +20,61 @@
 //     console.log(groceryList[index]);
 // }
 
-let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
 
-for (let nomes of names){
-    console.log(nomes);
-}
+// for (let nomes of names){
+//     console.log(nomes);
+// }
+
+
+// exercicio fatorial
+
+// let numero = 10
+// let fatorial = 1
+
+// for (let index = 1 ; index <= numero ; index +=1){
+//     fatorial = fatorial * index
+// } console.log("O fatorial de " + numero + " é: " + fatorial);
+
+// exercicio inverter palavra
+
+// let word = "Mathews";
+// let resposta = "";
+// for (let index = word.length -1 ; index >=0; index -=1){
+//     resposta += word[index]; 
+// } 
+// console.log(resposta);
+
+// exercicio maior e menor palavra
+
+let array = ['java', 'javaScript', 'python', 'html', 'css'];
+
+let maiorPalavra = array[0];
+let resultado = "";
+
+for (let index = 0; index < array.length; index += 1) {
+     if (array[index].length >  maiorPalavra){
+         maiorPalavra = array[index].length;
+     } 
+    
+} console.log(maiorPalavra);
+// for (let index2 = 0; index2 < array.length; index2 += 1){
+//     if(array[index2].length == maiorPalavra){
+//         resultado = maiorPalavra;
+//     }
+// }console.log(resultado);
+
+
+
+// Exercicio numeros primos
+
+// let primos = [];
+// let n = 50;
+
+// for (let index = 0; index <= n; index +=1);{
+//     primos[index] = index + 1;
+// }
+//     console.log(primos);
+//   // if (index % primos === 0 ) {
+
+    
