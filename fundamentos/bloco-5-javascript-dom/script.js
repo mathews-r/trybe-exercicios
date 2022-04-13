5bba5d1b737a1205cd036b2664816e120e34a684
Mathews <mathewsrodrigues9@gmail.com>
2022-04-13 17:47:20 -0300

    primeiros exercicios do dia

---
+++ fundamentos/bloco-5-javascript-dom/script.js
@@ -0,0 +1,17 @@
+let header = document.getElementById("header-container");
+header.style.backgroundColor = "green";
+
+let emergency = document.querySelector(".emergency-tasks");
+emergency.style.backgroundColor = "pink";
+
+let titulo = document.querySelector(".emergency-tasks h3");
+titulo.style.backgroundColor = "purple";
+
+let noemergency = document.querySelector(".no-emergency-tasks");
+noemergency.style.backgroundColor = "yellow";
+
+let titulo2 = document.querySelector(".no-emergency-tasks h3");
+titulo2.style.backgroundColor = "black";
+
+let rodape = document.getElementById("footer-container");
+rodape.style.backgroundColor = "green";
