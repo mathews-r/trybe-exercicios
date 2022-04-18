59f1329447608c50fdbbdbad911eb88306ba2a40
Mathews <mathewsrodrigues9@gmail.com>
2022-04-18 16:10:02 -0300

    feito o item 3, 4 e 5

--- fundamentos/bloco-5-javascript-dom/5.3/main.js
+++ fundamentos/bloco-5-javascript-dom/5.3/main.js
@@ -3,6 +3,7 @@ const secondLi = document.getElementById('second-li');
 const thirdLi = document.getElementById('third-li');
 const input = document.getElementById('input');
 const myWebpage = document.getElementById('my-spotrybefy');
+const classLi = document.getElementsByClassName('tech')[0];
 
 
 // 1. Copie esse arquivo e edite apenas ele;
@@ -15,21 +16,34 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
+input.addEventListener("input", function (event) {
+  classLi.innerText = event.target.value;
+});
+
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
 
+myWebpage.addEventListener("dblclick", function (event) {
+  window.location.replace("https://mathews-r.github.io/");
+});
+
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+myWebpage.addEventListener('mouseenter', function(event) {
+    event.target.style.color = "red";
+});
+
+
 
 // Segue abaixo um exemplo do uso de event.target:
 
 
 function resetText(event) {
-  // O Event é passado como um parâmetro para a função.
+//   // O Event é passado como um parâmetro para a função.
   event.target.innerText = 'Opção reiniciada';
-  // O event possui várias propriedades, porém a mais usada é o event.target,
-  // que retorna o objeto que disparou o evento.
+//   // O event possui várias propriedades, porém a mais usada é o event.target,
+//   // que retorna o objeto que disparou o evento.
 }
 
 firstLi.addEventListener('dblclick', resetText);
