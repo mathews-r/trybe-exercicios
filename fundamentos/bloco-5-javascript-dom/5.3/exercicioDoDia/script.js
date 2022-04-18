fd13fccda8e8ebeeaf0f3ca943af2107792e4984
Mathews <mathewsrodrigues9@gmail.com>
2022-04-18 19:21:08 -0300

    feito o exercicio 1

--- fundamentos/bloco-5-javascript-dom/5.3/exercicioDoDia/script.js
+++ fundamentos/bloco-5-javascript-dom/5.3/exercicioDoDia/script.js
@@ -13,4 +13,36 @@ function createDaysOfTheWeek() {
   
   createDaysOfTheWeek();
   
-  // Escreva seu código abaixo.
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  
+  function diasDoMes() {
+    const dias = document.querySelector('#days');
+
+    for (let index = 0; index < dezDaysList.length; index += 1) {
+      let arrayDias = dezDaysList[index];
+      let itemDia = document.createElement('li');
+
+      if (arrayDias === 24 || arrayDias === 31) {
+        itemDia.className = "day holyday";
+        itemDia.innerText = arrayDias;
+        dias.appendChild(itemDia);
+
+      } else if (arrayDias === 4 || arrayDias === 11 || arrayDias === 18) {
+        itemDia.className = "day friday";
+        itemDia.innerText = arrayDias;
+        dias.appendChild(itemDia);
+
+      } else if (arrayDias === 25) {
+        itemDia.className = "day holyday friday";
+        itemDia.innerText = arrayDias;
+        dias.appendChild(itemDia);
+
+      } else {
+        itemDia.innerText = arrayDias;
+        itemDia.className = 'day';
+        dias.appendChild(itemDia);
+
+    }}
+        
+    };
+    diasDoMes()
