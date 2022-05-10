8ca1a09e2ccff01a7b84b1f9736db5f4ecf57c6b
Mathews <mathewsrodrigues9@gmail.com>
2022-05-10 15:04:35 -0300

    exercicio 1 e 2 done

--- fundamentos/bloco-07-testes-unitarios/7.3/sum.test.js
+++ fundamentos/bloco-07-testes-unitarios/7.3/sum.test.js
@@ -1,9 +1,16 @@
 const sum = require('./sum');
 
-function verificaSoma() {
-  describe('Verifica a soma de a + b', () => {
+describe('Verifica a soma de a + b', () => {
     it('soma de a + b', () => {
-      expect(sum(4, 5)).tobe(9);
+      expect(sum(4, 5)).toBe(9);
     });
+    it('soma de 0 + 0 é 0', () => {
+      expect(sum(0, 0)).toBe(0);
+    });
+    it('deve disparar um erro caso receba como parâmetro uma string', () => {
+      expect(() => sum(4, '5')).toThrowError();
+      expect(() => sum(4, '5')).toThrowError('parameters must be numbers');
+    })
   });
-}
+
+sum(4, 5);
