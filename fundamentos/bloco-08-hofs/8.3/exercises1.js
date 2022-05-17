aa71450f3871a0bd25bface069a5a7312c98c5c6
Mathews <mathewsrodrigues9@gmail.com>
2022-05-17 19:19:53 -0300

    exercicio 2 finalizado

--- fundamentos/bloco-08-hofs/8.3/exercises1.js
+++ fundamentos/bloco-08-hofs/8.3/exercises1.js
@@ -71,33 +71,13 @@ console.log(formatedBookNames());
 
 // Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author, com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado. (.map, .sort)
 
-const expectedResult = [
-  {
-    age: 31,
-    author: 'Isaac Asimov',
-  },
-  {
-    age: 38,
-    author: 'H. P. Lovecraft',
-  },
-  {
-    age: 39,
-    author: 'Stephen King',
-  },
-  {
-    age: 43,
-    author: 'George R. R. Martin',
-  },
-  {
-    age: 45,
-    author: 'Frank Herbert',
-  },
-  {
-    age: 62,
-    author: 'J. R. R. Tolkien',
-  },
-];
+function nameAndAge(array) {
+  return array.map( (book) => {
+    return {
+      author: book.author.name,
+      age: book.releaseYear - book.author.birthYear,
+    }
+  })
+  }
 
-function nameAndAge() {
-  // escreva seu código aqui
-}
+console.log(nameAndAge(books));
