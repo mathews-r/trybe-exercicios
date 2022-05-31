9d75a8794030a09fb7e2d4dd5426905be433b382
Mathews <mathewsrodrigues9@gmail.com>
2022-05-31 15:57:19 -0300

    exercicios do dia 9.1

---
+++ frontEnd/9.1/test.js
@@ -0,0 +1,46 @@
+// const pushNumber = (list, number) => list.push(number);
+
+// let numbers = [];
+
+// pushNumber(numbers, 1);
+// pushNumber(numbers, 2);
+// pushNumber(numbers, 3);
+
+// console.log(numbers);
+
+// const pushNumber = (list, number) => list.push(number);
+
+// let numbers = [];
+
+// setTimeout(() => pushNumber(numbers, 1), 3000);
+// pushNumber(numbers, 2);
+// pushNumber(numbers, 3);
+
+// console.log(numbers); // O retorno é [2, 3]
+
+// const pushNumber = (list, number) => list.push(number);
+
+// let numbers = [];
+
+// setTimeout(() => pushNumber(numbers, 1), 3000);
+// pushNumber(numbers, 2);
+// pushNumber(numbers, 3);
+
+// setTimeout(() => console.log(numbers), 3000);
+
+const despesas = [
+  {
+    gym: 99,
+  },
+  {
+    ifood: 200,
+  },
+  {
+    phone: 60,
+  },
+  {
+    internet: 100,
+  },
+];
+
+const renda = 1000;
