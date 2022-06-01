9bd5c28456709640eaf751683c2f9b84dcf23214
Mathews <mathewsrodrigues9@gmail.com>
2022-06-01 20:11:42 -0300

    resolvendo ultimo exercicio

--- frontEnd/9.2/exercicios/apiScript.js
+++ frontEnd/9.2/exercicios/apiScript.js
@@ -1,15 +1,16 @@
 // apiScript.js
-const API_URL = 'https://icanhazdadjoke.com/';
+const url = 'https://icanhazdadjoke.com/';
 
-const fetchJoke = () => {
+const fetchJoke = async () => {
   const myObject = {
     method: 'GET',
     headers: { 'Accept': 'application/json' }
   };
 
-  fetch(API_URL, myObject)
+  await fetch(url, myObject)
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => document.getElementById('jokeContainer').innerText = data.joke);
+
 };
 
 window.onload = () => fetchJoke();
