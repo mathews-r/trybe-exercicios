527f102aa95d03bd6a4ccd12934576633acd8242
Mathews <mathewsrodrigues9@gmail.com>
2022-06-01 21:07:07 -0300

    refatorando codigo

--- frontEnd/9.2/exercicios/apiScript.js
+++ frontEnd/9.2/exercicios/apiScript.js
@@ -1,16 +1,23 @@
 // apiScript.js
 const url = 'https://icanhazdadjoke.com/';
 
+
+const append = (data) => {
+  const jokes = data.joke;
+  const container = document.getElementById("jokeContainer");
+  container.innerText = jokes;
+};
+
 const fetchJoke = async () => {
   const myObject = {
-    method: 'GET',
-    headers: { 'Accept': 'application/json' }
+    method: "GET",
+    headers: { Accept: "application/json" },
   };
 
   await fetch(url, myObject)
-    .then(response => response.json())
-    .then(data => document.getElementById('jokeContainer').innerText = data.joke);
-
+    .then((response) => response.json())
+    .then((data) => append(data))
+    .catch((error) => console.log(error));
 };
 
 window.onload = () => fetchJoke();
