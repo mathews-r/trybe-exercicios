906a7fada40923b990583e5cc5e539363fdc3dc3
Mathews <mathewsrodrigues9@gmail.com>
2022-06-20 20:52:11 -0300

    adicionando exercicio parte 2

---
+++ frontEnd/10.2/my-pokedex/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Pokemon from './Pokemon';
+
+
+class App extends React.Component {
+render() {
+  return (
+    <div>
+      <Pokemon />
+    </div>
+
+  );
+}
+
+}
+
+export default App;
---
+++ frontEnd/10.2/my-pokedex/src/Pokedex.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Pokemon from './Pokemon';
+
+class Pokedex extends React.Component {
+    render() {
+      return ();
+    }
+}
+
+export default Pokedex;
