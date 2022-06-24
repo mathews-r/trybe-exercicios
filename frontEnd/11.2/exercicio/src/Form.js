f9d69ba4578a3c5475cac91bfc6f9c704d663acd
Mathews <mathewsrodrigues9@gmail.com>
2022-06-23 21:10:38 -0300

    requisito

--- frontEnd/11.2/exercicio/src/Form.js
+++ frontEnd/11.2/exercicio/src/Form.js
@@ -1,29 +1,44 @@
 import React from 'react';
 
 class Form extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      name: '',
+      email: '',
+      cpf: '',
+      adress: '',
+      city: '',
+      curriculo: '',
+      cargo: '',
+      descricaoCargo: '',
+    }
+  }
+  
   render() {
     return (
       <form>
         <fieldset>
           <label>
             Nome: 
-            <input type='text' maxLength='40' required />
+            <input name='name' type='text' maxLength='40' required />
           </label>
           <label>
             Email:
-            <input type='text' maxLength='50' required />
+            <input name='email' type='text' maxLength='50' required />
           </label>
           <label>
             CPF:
-            <input type='text' maxLength='11' required />
+            <input name='cpf' type='text' maxLength='11' required />
           </label>
           <label>
             Endereço:
-            <input type='text' maxLength='200' required />
+            <input name='adress' type='text' maxLength='200' required />
           </label>
           <label>
             Cidade:
-            <input type='text' maxLength='28' required />
+            <input name='city' type='text' maxLength='28' required />
           </label>
 
           <label htmlFor='radioHome'>
@@ -36,15 +51,15 @@ class Form extends React.Component {
         <fieldset>
           <label>
             Resumo do curriculo:
-            <textarea type="text" maxLength="1000" required></textarea>
+            <textarea name='curriculo' type="text" maxLength="1000" required></textarea>
           </label>
           <label>
             Cargo:
-            <textarea type="text" maxLength="40" required></textarea>
+            <textarea  name='cargo' type="text" maxLength="40" required></textarea>
           </label>
           <label>
             Descrição do cargo:
-            <input type='text' maxLength='50' required />
+            <input name='descricaoCargo' type='text' maxLength='50' required />
           </label>
         </fieldset>
         <button type='submit'>Salvar</button>
